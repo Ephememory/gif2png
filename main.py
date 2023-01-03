3e8f6c828aed0fee56408425392d2c64f5050792
Ephememory <leetvipertm@gmail.com>
2023-01-03 13:18:14 -0600

    use imagesequence instead

--- main.py
+++ main.py
@@ -2,34 +2,37 @@ import sys
 import io
 import os
 from PIL import Image
+from PIL import ImageSequence
 
 
-def main(dir, contents) -> None:
-    print(dir)
-    for i, path in enumerate(contents):
-        abs_path = dir + "\\" + path
+def main(dir, contents, output_dir_prefix) -> None:
+    if output_dir_prefix is not None and len(output_dir_prefix) > 0:
+        destination_dir = dir + "\\" + output_dir_prefix + "_" + "sheets"
+    else:
+        destination_dir = dir + "\\" + "sheets"
+
+    if not os.path.exists(destination_dir):
+        os.mkdir(destination_dir)
+
+    for i, filename in enumerate(contents):
+        abs_path = dir + "\\" + filename
         with Image.open(abs_path) as gif:
             print(f"Processing: {abs_path} image mode: {gif.mode}")
 
             if not gif.is_animated or gif.n_frames <= 0:
                 continue
 
-            sprite_sheet = Image.new(mode=gif.mode, size=(
+            sprite_sheet = Image.new(mode="RGBA", size=(
                 gif.width * gif.n_frames, gif.height))
-            sprite_sheet.putpalette(gif.getpalette())
 
-            for n_frame in range(gif.n_frames):
-                for y in range(gif.height):
-                    for x in range(gif.width):
-                        pixel = gif.getpixel((x, y))
-                        print(f"{path} : {pixel}")
-                        # if type(pixel) is tuple and len(pixel) >= 4 and pixel[3] <= 0:
-                        #     pixel = (0, 0, 0)
-                        dest_x = x + gif.width * n_frame
-                        sprite_sheet.putpixel((dest_x, y), pixel)
-                gif.seek(n_frame)
-            sprite_sheet_path = path.replace(".gif", ".png")
-            sprite_sheet.save(sprite_sheet_path)
+            n_frame = 0
+            for frame in ImageSequence.Iterator(gif):
+                sprite_sheet.paste(
+                    frame, (n_frame * frame.width, 0))
+                n_frame += 1
+
+            sheet_filename = filename.replace(".gif", ".png")
+            sprite_sheet.save(destination_dir + "\\" + sheet_filename)
 
 
 if __name__ == "__main__":
@@ -41,10 +44,14 @@ if __name__ == "__main__":
         exit()
 
     desired_dir = sys.argv[1]
+
     if os.path.isdir(desired_dir):
         contents = os.listdir(desired_dir)
         if contents == None or contents == []:
             print("No files in desired directory! Exiting.")
             exit()
         dir = desired_dir
-        main(dir, contents)
+        output_prefix = None
+        if argc >= 3:
+            output_prefix = sys.argv[2]
+        main(dir, contents, output_prefix)
